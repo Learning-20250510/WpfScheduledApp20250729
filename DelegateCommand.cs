762f747fd3cc1cbd202aa689b3b660b68ac3e591
DESKTOP-GRLAOLR\tugar <luny8bb4@gmail.com>
2025-07-30 00:55:46 +0900

    DelegateCommand,NotificationObjectを追加。

---
+++ DelegateCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfScheduledApp20250629
+{
+    internal class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// この２つはデリゲートつまり、methodが登録しておける入れ物、so,要所要所でNullとかでるのは
+        /// 登録されてなかったらこのでりげーと実行する必要がないからそこで条件式かいてるのかな？エラー出るのかな？
+        /// </summary>
+        private Action<object> _execute;
+
+        private Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="execute"></param>
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// デリゲート初動で記述したのだから、何か登録しないと！そこでコンストラクタでインスタンス呼ばれた時に登録されるようにしている
+        /// あと、CanExecute内部で、自身の登録先のデリゲートを評価することもできるのか
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+
+            return _canExecute != null ? _canExecute(parameter) : true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var h = CanExecuteChanged;
+
+            if (h != null) h(this, EventArgs.Empty);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+                _execute(parameter);
+        }
+
+    }
+}
---
+++ NotificationObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfScheduledApp20250629
+{
+    internal abstract class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var h = PropertyChanged;
+            if (h != null) h(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        protected bool SetProperty<T>(ref T target, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(target, value))
+            {
+                return false;
+            }
+
+            target = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+
+        }
+    }
+}
