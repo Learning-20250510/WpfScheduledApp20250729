78c61f78b7d6b03c961ea949ff1137e3db77b638
DESKTOP-GRLAOLR\tugar <luny8bb4@gmail.com>
2025-08-04 00:01:46 +0900

    DelegateCommandをC#8.0対応に修正。（null制約など）

--- DelegateCommand.cs
+++ DelegateCommand.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace WpfScheduledApp20250729
@@ -13,55 +9,46 @@ namespace WpfScheduledApp20250729
         /// この２つはデリゲートつまり、methodが登録しておける入れ物、so,要所要所でNullとかでるのは
         /// 登録されてなかったらこのでりげーと実行する必要がないからそこで条件式かいてるのかな？エラー出るのかな？
         /// </summary>
-        private Action<object> _execute;
-
-        private Func<object, bool> _canExecute;
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
         /// <param name="execute"></param>
-        public DelegateCommand(Action<object> execute)
+        public DelegateCommand(Action<object?> execute)
             : this(execute, null)
         {
-
         }
 
-
-
         /// <summary>
-        /// 
         /// デリゲート初動で記述したのだから、何か登録しないと！そこでコンストラクタでインスタンス呼ばれた時に登録されるようにしている
         /// あと、CanExecute内部で、自身の登録先のデリゲートを評価することもできるのか
         /// </summary>
         /// <param name="execute"></param>
         /// <param name="canExecute"></param>
-        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        public DelegateCommand(Action<object?> execute, Func<object?, bool>? canExecute)
         {
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
-
             return _canExecute != null ? _canExecute(parameter) : true;
         }
 
-        public event EventHandler CanExecuteChanged;
+        // イベントを空のデリゲートで初期化（null警告を回避）
+        public event EventHandler? CanExecuteChanged = delegate { };
 
         public void RaiseCanExecuteChanged()
         {
-            var h = CanExecuteChanged;
-
-            if (h != null) h(this, EventArgs.Empty);
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
 
-        public void Execute(object parameter)
+        public void Execute(object? parameter)
         {
-            if (_execute != null)
-                _execute(parameter);
+            _execute(parameter);
         }
-
     }
 }
